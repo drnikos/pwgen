c743957283b9aae2ad7375294015a42d17641992
DrNikos <drnikos@proton.me>
2023-12-12 23:44:58 +0200

    Made the generated password colored

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod randomgen;
 use randomgen::randomgen::{passgen, passinfo};
 fn main() {
     let x = passinfo();
-    println!("Your password is: {}", passgen(x));
+    println!("Your password is: \x1b[93m{}\x1b[0m", passgen(x));
 }
